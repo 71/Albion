71f1fc6fdeda910c7982fe9727df25381fe32e66
Greg G <m-ae.j@outlook.com>
2017-03-19 11:31:59 +0100

    Switched to .NET Core

--- up.bat
+++ up.bat
@@ -5,8 +5,8 @@ SET	MSB="C:\Program Files (x86)\MSBuild\14.0\Bin\msbuild.exe"
 
 MKDIR	nuget\lib\portable-net45+netcore45+win8+wp8+MonoAndroid+Xamarin.iOS10+MonoTouch>nul
 
-%MSB%	Albion\Albion.csproj /property:Configuration=Release;OutDir="..\nuget\lib\portable-net45+netcore45+win8+wp8+MonoAndroid+Xamarin.iOS10+MonoTouch">nul
+%MSB%	Albion\Albion.csproj /property:Configuration=Release;OutDir="..\nuget\lib\portable-net45+netcore45+win8+wp8+MonoAndroid+Xamarin.iOS10+MonoTouch"
 COPY	Albion.nuspec nuget>nul
 
 NUGET	pack nuget\Albion.nuspec  -OutputDirectory .>nul
-RMDIR	nuget /S /Q>nul
+REM RMDIR	nuget /S /Q>nul
